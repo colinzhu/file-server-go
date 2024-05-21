633caf06a06af21ed5f074cd9f1d1d77d2ed6f44
colinzhu <colin.x.z.zhu+github@gmail.com>
2024-05-21 23:33:16 +0800

    Show IP in the log

--- main.go
+++ main.go
@@ -12,33 +12,35 @@ func main() {
 	startServer()
 }
 
-// func startServer() {
-// 	fs := http.FileServer(http.Dir("./"))
-// 	http.Handle("/", fs)
-
-// 	//log.Print("Listening on :8888...")
-// 	err := http.ListenAndServe(":0", nil)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// }
 func startServer() {
+
 	port := flag.Int("p", 0, "Port number to use, default is 0 for random")
 	dir := flag.String("d", "./", "Directory to serve")
 	flag.Parse()
 
-	fs := http.FileServer(http.Dir(*dir))
-	http.Handle("/", fs)
-
-	listener, err := net.Listen("tcp", fmt.Sprint(":", *port))
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer listener.Close()
 
-	log.Printf("Listening on %s...", listener.Addr())
-	err = http.Serve(listener, nil)
+	// Get the IP addresses of the current machine
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Print the IP addresses
+	log.Println("URL:")
+	for _, addr := range addrs {
+		ip := addr.(*net.IPNet).IP
+		if ip.IsLoopback() {
+			continue
+		}
+		log.Printf("http://%s:%d\n", ip, listener.Addr().(*net.TCPAddr).Port)
+	}
+
+	err = http.Serve(listener, http.FileServer(http.Dir(*dir)))
 	if err != nil {
 		log.Fatal(err)
 	}
